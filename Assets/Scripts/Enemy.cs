00931285aa23c3aede160df8c06d6bd1bdf3a8f2
ryandicenzo <dicenzoryan@gmail.com>
2016-07-06 19:23:41 -0700

    Added UI template
    Non functional

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
- 
-public class Enemy : MonoBehaviour {
+
+public class Enemy : MonoBehaviour
+{
     public float Health;
     public LayerMask enemyMask;
     Transform myTrans;
@@ -10,39 +11,30 @@ public class Enemy : MonoBehaviour {
     float MyWidth;
     public float speed;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Health = 20;
         myTrans = this.transform;
         mybody = this.GetComponent<Rigidbody2D>();
         MyWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
     }
-   
+
     // Update is called once per frame
-    void FixedUpdate () {
-    Vector2 lineCastPos = myTrans.position - myTrans.right * MyWidth;
-    Debug.DrawLine(lineCastPos, lineCastPos - Vector2.up);
-    bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos - Vector2.up, enemyMask);
+    void FixedUpdate()
+    {
+        Vector2 lineCastPos = myTrans.position - myTrans.right * MyWidth;
+        Debug.DrawLine(lineCastPos, lineCastPos - Vector2.up);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos - Vector2.up, enemyMask);
 
-    if(!isGrounded){
-        Vector3 currRot = myTrans.eulerAngles;
-        currRot.y += 180;
-        myTrans.eulerAngles = currRot;
-    }
-    //Always moves forward
-    Vector2 myVel = mybody.velocity;
-    myVel.x = -myTrans.right.x * speed;
-    mybody.velocity = myVel;        
-
-
-
-
-
-
-    //Health
-        if(Health <= 0){
-            Destroy(gameObject);
+        if (!isGrounded)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
         }
-       
+        //Always moves forward
+        Vector2 myVel = mybody.velocity;
+        myVel.x = -myTrans.right.x * speed;
+        mybody.velocity = myVel;
     }
- 
-    }
+}
