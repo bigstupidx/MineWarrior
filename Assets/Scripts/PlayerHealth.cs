4b28aadeb6624c40cc88f31a16ab3311769f1761
ryandicenzo <dicenzoryan@gmail.com>
2016-07-09 00:41:10 -0700

    Health HUD

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour 
+{
+    [SerializeField]
+    private float fillAmount;
+    [SerializeField]
+    private Image HUD;
+
+    private float playerHealth = 100f;
+    
+    private CharController charController;
+
+    void Awake()
+    {
+        charController = GetComponent<CharController>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+        fillAmount = playerHealth / 100;
+
+        if (playerHealth <= 0)
+        {
+            charController.Die();
+        }
+    }
+
+    void Update()
+    {
+        HandleBar();
+    }
+
+    void HandleBar()
+    {
+        HUD.fillAmount = fillAmount;
+    }
+}
