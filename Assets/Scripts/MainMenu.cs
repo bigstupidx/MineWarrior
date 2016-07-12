473d3c0b66438b283cd2b00bdc875bd7d5ac3a96
ashton <djminni@live.com>
2016-07-12 03:37:38 -0600

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Play()
+    {
+        Application.LoadLevel("Level1");
+    }
+    public void LevelSelect()
+    {
+        Application.LoadLevel("LevelSelect");
+    }
+    public void Options()
+    {
+        Application.LoadLevel("Options");
+    }
+}
