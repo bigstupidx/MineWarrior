d453c20b66f080153bc799b92b443f3d280e69bc
ryandicenzo <dicenzoryan@gmail.com>
2016-07-11 22:11:36 -0700

    Player Flicker

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,12 +8,15 @@ public class PlayerHealth : MonoBehaviour
     private float fillAmount;
     [SerializeField]
     private Image HUD;
+    [SerializeField]
+    private float playerHealth = 100f;
 
-    private float playerHealth = 100f;    
+    private SpriteRenderer playerSpriteRenderer;
     private CharController charController;
 
     void Awake()
     {
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
         charController = GetComponent<CharController>();
     }
 
@@ -26,6 +29,10 @@ public class PlayerHealth : MonoBehaviour
         {
             charController.Die();
         }
+        else
+        {
+            StartCoroutine(FadePlayer());
+        }
     }
 
     void Update()
@@ -36,5 +43,22 @@ public class PlayerHealth : MonoBehaviour
     void HandleHUD()
     {
         HUD.fillAmount = fillAmount;
+    }
+
+    IEnumerator FadePlayer()
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            for (float i = 1f; i >= 0; i -= 0.05f)
+            {
+                yield return new WaitForSeconds(.01f);
+                playerSpriteRenderer.color = new Color(1f, 1f, 1f, i);
+            }
+            for (float i = 0f; i <= 1; i += 0.05f)
+            {
+                yield return new WaitForSeconds(.01f);
+                playerSpriteRenderer.color = new Color(1f, 1f, 1f, i);
+            }        
+        }
     }
 }
