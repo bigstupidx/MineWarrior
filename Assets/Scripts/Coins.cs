f2e394a8d6f39472235c4f9881c5c80edb89e5bc
ashton <djminni@live.com>
2016-07-16 03:52:37 -0600

    Score

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 
 public class Coins : MonoBehaviour 
-{ 
+{
+    public Scores sc;
     [SerializeField]
     GameObject coinEffect;
 
 	void OnTriggerEnter2D(Collider2D coll)
-    {                
+    {
+        sc.GetComponent<Scores>();
+
         if(coll.gameObject.tag == "Player")
         {
             Destroy(this.gameObject);
             Destroy(Instantiate(coinEffect, transform.position, transform.rotation), 1f);
+            sc.scores += 10;
         }
     }
 }
