efe9760321dcfa6ccc2981a89a5f37a09f86c76e
ryandicenzo <dicenzoryan@gmail.com>
2016-07-25 22:50:54 -0700

    minor changes

---
+++ Assets/Scripts/Interactables/TNT.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TNT : MonoBehaviour 
+{
+    [SerializeField]
+    GameObject _explosion;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Bullet")
+        {
+            Destroy(Instantiate(_explosion, coll.gameObject.transform.position, Quaternion.identity), 3);
+            Destroy(this.gameObject);
+        }
+    }
+}
